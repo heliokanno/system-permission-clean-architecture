2ba894960cb4c06853efd4819153d3f01d245444
helio <heliok.dev@gmail.com>
2023-10-03 17:46:07 +0100

    initial commit

---
+++ application/src/main/java/com/hmk/system/permission/application/SystemPermissionApplication.java
@@ -0,0 +1,14 @@
+package com.hmk.system.permission.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SystemPermissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemPermissionApplication.class, args);
+    }
+
+}
---
+++ application/src/test/java/com/hmk/system/permission/application/SystemPermissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmk.system.permission.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemPermissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
